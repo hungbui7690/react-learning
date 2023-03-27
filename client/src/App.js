7cf04542295e23c1709cdbad5cf0c9d190017075
hungbui7690 <hungbui7690@gmail.com>
2023-03-28 02:15:19 +0700

    More on State

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
 /*
-  Introducing the State System
+  More on State
   - pic
 
 */
 
-import { useState } from 'react' // ***
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0) // ***
+  const [count, setCount] = useState(0)
 
   const handleClick = () => {
-    setCount(count + 1) // ***
+    setCount(count + 1)
   }
 
   return (
     <div className='App'>
       <button onClick={handleClick}>Add Animal</button>
-
-      {/* *** */}
       <div>Number of Animals: {count}</div>
     </div>
   )
