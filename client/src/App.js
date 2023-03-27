fd4771ca20fffc4e4a18eedeb6a24bf15786f5a6
hungbui7690 <hungbui7690@gmail.com>
2023-03-27 18:36:21 +0700

    Handling Image Accessibility

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 /*
-  Including Images P2
-  - pic
+  Handling Image Accessibility
 
-  (***) the way we handle fonts and media/sound are similar
 */
 
 import ProfileCard from './ProfileCard'
@@ -11,22 +9,14 @@ import CortanaImage from './images/cortana.png'
 import SiriImage from './images/siri.png'
 
 function App() {
-  // ***
   return (
     <div>
       <div>Personal Digital Assistants</div>
 
-      {/* local */}
-      <img src={AlexaImage} alt='Alexa' />
-      <img src={SiriImage} alt='Siri' />
-      <img src={CortanaImage} alt='Cortana' />
-
-      {/* external  */}
-      <img src='https://picsum.photos/200' alt='random' />
-
-      <ProfileCard title='Alexa' handle='@alexa99' />
-      <ProfileCard title='Cortana' handle='@acortana32' />
-      <ProfileCard title='Siri' handle='@siri01' />
+      {/* pass props: image */}
+      <ProfileCard image={AlexaImage} title='Alexa' handle='@alexa99' />
+      <ProfileCard image={CortanaImage} title='Cortana' handle='@acortana32' />
+      <ProfileCard image={SiriImage} title='Siri' handle='@siri01' />
     </div>
   )
 }
