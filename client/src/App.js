034e83048a407f9b43ca7082074f558b51e66cf5
hungbui7690 <hungbui7690@gmail.com>
2023-03-27 18:13:27 +0700

    Creating Core Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 /*
-  Overview
-  - pic
+  Creating Core Components
+  - create ProfileCard.js
 
 */
 
+import ProfileCard from './ProfileCard' // ***
+
 function App() {
-  return <div className='App'>App</div>
+  // ***
+  return (
+    <div>
+      <div>Personal Digital Assistants</div>
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+    </div>
+  )
 }
 
 export default App
