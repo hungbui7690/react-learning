75180a3d52c8a5f0a4d89a3185e06f8d1f5259d2
hungbui7690 <hungbui7690@gmail.com>
2023-03-28 02:00:23 +0700

    Initial App Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 /*
-  
+  App Overview
+  - pic
+
+//////////////////////////////
+
+  Initial App Setup
+  - pic
+
+  - create AnimalShow.js
 
 */
 
 function App() {
-  return <div className='App'>App</div>
+  return <div className='App'>Animal List Here</div>
 }
 
 export default App
