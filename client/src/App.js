4a7e5ecd1d60a067b26449787d31af5d95f7e7ff
hungbui7690 <hungbui7690@gmail.com>
2023-03-27 18:33:31 +0700

    Including Images P2

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
 /*
-  Including Images P1
+  Including Images P2
   - pic
-  - create src/images/
 
-  (***) depends on the size of the image, it will be used differently > webpack
-    - small: raw data
-    - big: image file
+  (***) the way we handle fonts and media/sound are similar
 */
 
 import ProfileCard from './ProfileCard'
-
-// ***
 import AlexaImage from './images/alexa.png'
 import CortanaImage from './images/cortana.png'
 import SiriImage from './images/siri.png'
 
-// ***
-console.log(AlexaImage)
-console.log(SiriImage)
-
 function App() {
+  // ***
   return (
     <div>
       <div>Personal Digital Assistants</div>
+
+      {/* local */}
+      <img src={AlexaImage} alt='Alexa' />
+      <img src={SiriImage} alt='Siri' />
+      <img src={CortanaImage} alt='Cortana' />
+
+      {/* external  */}
+      <img src='https://picsum.photos/200' alt='random' />
+
       <ProfileCard title='Alexa' handle='@alexa99' />
       <ProfileCard title='Cortana' handle='@acortana32' />
       <ProfileCard title='Siri' handle='@siri01' />
