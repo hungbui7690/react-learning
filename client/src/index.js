20c51300a6b4a6d75dab23ce2163d3f8d6308d48
hungbui7690 <hungbui7690@gmail.com>
2023-03-27 16:21:22 +0700

    What is JSX

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,17 @@
 /*
-  Showing Basic Content
+  What is JSX
   - pic
-  - delete un-necessary files: 
-    + public/
-      > delete everything, but keeps index.html
-    + src/
-      > delete everything > create index.js
 
 */
 
-// (1) import React & ReactDOM Libraries
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// (2) Get the reference to div with ID of root
 const el = document.getElementById('root')
-
-// (3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el)
 
-// (4) Create a component
 function App() {
   return <h1>Hi There!</h1>
 }
 
-// (5) Show the component on the screen
 root.render(<App />)
