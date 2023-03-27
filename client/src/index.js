1d5423250d329f6ffad4e05c1e876544bea51df9
hungbui7690 <hungbui7690@gmail.com>
2023-03-27 16:19:05 +0700

    Showing Basic Content

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,28 @@
+/*
+  Showing Basic Content
+  - pic
+  - delete un-necessary files: 
+    + public/
+      > delete everything, but keeps index.html
+    + src/
+      > delete everything > create index.js
+
+*/
+
+// (1) import React & ReactDOM Libraries
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// (2) Get the reference to div with ID of root
+const el = document.getElementById('root')
+
+// (3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el)
+
+// (4) Create a component
+function App() {
+  return <h1>Hi There!</h1>
+}
+
+// (5) Show the component on the screen
+root.render(<App />)
