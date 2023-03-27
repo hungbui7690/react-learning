8f614443318a6900b9ea32900088c268f889ff5e
hungbui7690 <hungbui7690@gmail.com>
2023-03-27 18:18:51 +0700

    Adding Props

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
 /*
-  Creating Core Components
-  - create ProfileCard.js
+  Introducing the Props System
+  - pic
 
+/////////////////////////////
+
+  Picturing the Movement of Data
+  - pic
+
+/////////////////////////////
+
+  Adding Props
+  
 */
 
-import ProfileCard from './ProfileCard' // ***
+import ProfileCard from './ProfileCard'
 
 function App() {
-  // ***
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+
+      {/* (1) */}
+      <ProfileCard title='Alexa' handle='@alexa99' />
+      <ProfileCard title='Cortana' handle='@acortana32' />
+      <ProfileCard title='Siri' handle='@siri01' />
     </div>
   )
 }
--- client/src/ProfileCard.js
+++ client/src/ProfileCard.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
-const ProfileCard = () => {
-  return <div>Profile Card</div>
+// (2a)
+const ProfileCard = (props) => {
+  // (2b)
+  return (
+    <div>
+      <div>Title is {props.title}</div>
+      <div>Handle is {props.handle}</div>
+    </div>
+  )
 }
 
 export default ProfileCard
