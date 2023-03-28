a9dd0fdd60bf9a9c43cfd8412d85060289897718
hungbui7690 <hungbui7690@gmail.com>
2023-03-28 10:36:46 +0700

    Receiving New Titles

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,30 @@
   Reminder on Event Handlers
   - pic
 
+////////////////////////////////////
+
+  Receiving New Titles
+  - copy file index.css
+
 */
 
 import { useState } from 'react'
+import BookCreate from './components/BookCreate' // ***
 
 function App() {
   const [books, setBooks] = useState([])
 
-  return <div className='App'>App</div>
+  // ***
+  const createBook = (title) => {
+    console.log('Need to add book with title: ', title)
+  }
+
+  return (
+    <div>
+      {/* *** pass props => can use onCreate or onSubmit  */}
+      <BookCreate onCreate={createBook} />
+    </div>
+  )
 }
 
 export default App
--- client/src/components/BookCreate.js
+++ client/src/components/BookCreate.js
@@ -1,7 +1,35 @@
-import React from 'react'
+/*
+  - create form
+  - create handleSubmit
+*/
 
-const BookCreate = () => {
-  return <div>BookCreate</div>
+import { useState } from 'react'
+
+// ***
+const BookCreate = ({ onCreate }) => {
+  const [title, setTitle] = useState('')
+
+  // *** use below
+  const handleChange = (e) => {
+    setTitle(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onCreate(title) // ***
+
+    setTitle('') // empty out the input
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input type='text' value={title} onChange={handleChange} />
+        <button type='submit'>Add Book</button>
+      </form>
+    </div>
+  )
 }
 
 export default BookCreate
