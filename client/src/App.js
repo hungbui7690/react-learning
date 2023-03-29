f8752e3f1f66db756daf3e1d6a1b48cb59cfd913
hungbui7690 <hungbui7690@gmail.com>
2023-03-29 14:22:45 +0700

    ESLint is Good but be Careful

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,32 @@
 /*
-  Applying the Fix
-  - pic
+  ESLint is Good but be Careful
+  - we add the fix to our app
+    > first glance > our app works fine
+    > but if we check network tab > requests was sent continuously
+    > why > pic
   
-  
+
   *** https://codesandbox.io/s/hungry-fog-0ev1ec
 
 */
 
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
+import BookCreate from './components/BookCreate'
+import BookList from './components/BookList'
+import { useBooksContext } from './hooks/use-books-context'
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const { fetchBooks } = useBooksContext()
 
   useEffect(() => {
-    document.body.onclick = () => {
-      console.log(counter)
-    }
-  }, [counter]) // *** the warning says that we need to add counter here
+    fetchBooks()
+  }, [fetchBooks]) // *** we add the fix here
 
   return (
-    <div>
-      <button onClick={() => setCounter(counter + 1)}>+ Increment</button>
-      <div>Count: {counter}</div>
+    <div className='app'>
+      <h1>Reading List</h1>
+      <BookList />
+      <BookCreate />
     </div>
   )
 }
