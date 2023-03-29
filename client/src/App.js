7da1b6528a682a8781fc68b03e93e20317b12f7d
hungbui7690 <hungbui7690@gmail.com>
2023-03-29 14:11:23 +0700

    Understanding the Issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 /*
-  Return to useEffect P3
-  - with this setup below, when we click on the button > it still increases the counter 
-  - but the problem is when we click anywhere else > it still logs out 0 though we logged counter
+  Understanding the Issue
+  - pic
   
   
   *** https://codesandbox.io/s/hungry-fog-0ev1ec
@@ -13,7 +12,6 @@ import { useState, useEffect } from 'react'
 function App() {
   const [counter, setCounter] = useState(0)
 
-  // *** DON'T DO THIS > if we change to "counter" > warning
   useEffect(() => {
     document.body.onclick = () => {
       console.log(counter)
