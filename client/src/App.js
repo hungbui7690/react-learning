cf0b6ee57a5679fe38932020f0a25f436da12b0c
hungbui7690 <hungbui7690@gmail.com>
2023-03-30 00:01:18 +0700

    Introducing TailwindCSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,8 @@
 /*
-  PropTypes in Action P2 (***)
-  - turn true/false/undefined into number
-    - Number(true) = 1
-    - Number(undefined) = NaN
-    - Number(!!undefined) = 0
-    - !!undefined = false 
-
-  *** prop-types does not show error, it just show WARNING
-    > nowadays, TS can do this for us
-
+  Introducing TailwindCSS
+  - pic (***)
+  - css library
+    > each class name has singular styling rule
 */
 
 import Button from './Button'
